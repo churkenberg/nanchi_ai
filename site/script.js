6bf0862799b81aed8f8d6070fb2ba24906f7e83b
Churkenberg <134487589+churkenberg@users.noreply.github.com>
2024-09-13 13:16:21 +0500

    update site

--- site/script.js
+++ site/script.js
@@ -1,3 +1,5 @@
 document.getElementById('cta-button').addEventListener('click', () => {
     alert('Button clicked!');
 });
+console.log('Navigation bar styles:', document.querySelector('nav').style.display);
+console.log('Tab container styles:', document.querySelector('.navigation-tabs').style.display);
